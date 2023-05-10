8eeb238a7e8f7dc786b4a565c7346f0d2631198d
Valentino Valle <valentino123.valle@gmail.com>
2023-05-10 20:13:52 -0300

    started layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { TestimonialsSectionComponent } from './testimonials-section/testimonials-section.component';
+import { GetStartedBtnComponent } from './get-started-btn/get-started-btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainSectionComponent,
+    PageFooterComponent,
+    TestimonialComponent,
+    TestimonialsSectionComponent,
+    GetStartedBtnComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/get-started-btn/get-started-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-started-btn',
+  templateUrl: './get-started-btn.component.html',
+  styleUrls: ['./get-started-btn.component.scss']
+})
+export class GetStartedBtnComponent {
+
+}
