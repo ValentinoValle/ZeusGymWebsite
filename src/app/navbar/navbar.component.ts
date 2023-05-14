29eabc68dd16e98a51e165992307236d84d3c8ab
Valentino Valle <valentino123.valle@gmail.com>
2023-05-14 19:00:43 -0300

    nose

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  navLinks: string[] = ["Home", "Routines", "About Us", "Contact"]
 }
