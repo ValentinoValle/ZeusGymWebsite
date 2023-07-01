de3e1b691c9e6a622794870a43ca527d452e0155
Valentino Valle <valentino123.valle@gmail.com>
2023-07-01 16:15:21 -0300

    added animations to routine builder tables

--- src/app/routine-table/routine-table.component.ts
+++ src/app/routine-table/routine-table.component.ts
@@ -9,12 +9,12 @@ import { animate, style, transition, trigger } from '@angular/animations';
   animations: [
     trigger('enterLeave', [
       transition(':enter', [
-        style({ opacity: 0 }),
-        animate(200),
+        style({ opacity: 0, transform: 'translateX(-100%)' }),
+        animate(".2s ease-in-out", style({ opacity: 1, transform: 'translateX(0)' })),
       ]),
       transition(':leave', [
         style({ opacity: 1 }),
-        animate(200, style({ opacity: 0, transform: 'translateX(-100%)' })),
+        animate(".2s ease-in-out", style({ opacity: 0, transform: 'translateX(-100%)' })),
       ]),
     ]),
   ],
